419c62992ea0ebc42e665185d1ef6246894c5649
Kur0r0 <limaalyson@hotmail.com>
2022-01-10 00:25:45 -0300

    testes com tipos e o gif do readme

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std  ::vec;
+use Coisa::*  ;
+
+
+struct  CoisaStru{
+    Cor   : String,
+    Number: i32
+}
+enum Coisa{
+    Cor(String),
+    Number(i32)
+}
 fn main() {
-    println!("Hello, world!!E meus queridos testemunhas ");
+
+    //println!("Hello, world!!E meus queridos testemunhas ");
+    let _num64 = 32f64;
+    //let _cores = vec![12.,13.] ; 
+    let _cores = vec![Cor("greem".to_string()) , Number(32)];     //let mut _cores = vec![Coisa::Cor(13),Coisa::Number(12)];
+
+    println!("o número é {}\na cor é {}",_cores[1],_cores[0]);
+    //_cores = vec![Coisa::Number(12) , Coisa::Number(12)];
+    // println!("isso aqui {}",_cores[0]);
 }
