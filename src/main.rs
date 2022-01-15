d577ff4e906f2c6151f93fb42ba809f1ea612437
Kur0r0 <limaalyson@hotmail.com>
2022-01-15 01:27:51 -0300

    update field e game_session

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use core::fmt;
-use std::{  vec, fmt::write};
+use std::{ vec};
 use Coisa::{Cor , Number}  ;
+use rand::Rng;
+use Direction::{left,right ,up ,down};
 
 enum Direction{
     left ,
@@ -9,8 +11,8 @@ enum Direction{
     down,
 }
 struct position{
-    x  : i32,
-    y  : i32
+    x  : u16,
+    y  : u16
 }
 struct snake{
     body : Vec<position>,
@@ -47,13 +49,46 @@ impl snake {
     }
 }
 struct field{
-    size_X:i8,
-    size_y:i8,
+    size_X:u16,
+    size_y:u16,
     food_Pos:position
 }
 impl field {
-    fn new_Food(self){
-        println!("Eu gero um número aleatório entre 1 e size_x e size_y e guardo como food_Pos");        
+    fn new_Food(self , cobra_body :Vec<position>){
+        let mut rng = rand::thread_rng();
+        let mut new_food = position{x : rng.gen_range(1..self.size_X) , y : rng.gen_range(1..self.size_y) } ;
+        loop {
+            if  !cobra_body.contains(&new_food)   {
+                break;
+            }
+            let mut new_food = position{x : rng.gen_range(1..x_field_size) , y : rng.gen_range(1..y_field_size) } ;
+        }
+        field{food_Pos : new_food , ..self};
+        
+    }
+}
+struct game_session{
+    points : u16,
+    record : u16,
+    player : snake,
+    ground : field,
+}
+impl game_session {
+    pub fn new(x_field_size :u16 ,y_field_size :u16 )-> Self {
+        let mut rng = rand::thread_rng();
+        let cobra = snake{body : vec![position{x:rng.gen_range(1..x_field_size-1) , y : rng.gen_range(1..y_field_size)}] ,
+        lastDirection : right };
+        let mut new_food = position{x : rng.gen_range(1..x_field_size) , y : rng.gen_range(1..y_field_size) } ;
+        loop {
+            if !cobra.body.contains(&new_food) {
+                break;
+            }
+            let mut new_food = position{x : rng.gen_range(1..x_field_size) , y : rng.gen_range(1..y_field_size) } ;
+        }
+        
+        let land = field { size_X : x_field_size  , size_y : y_field_size, food_Pos : new_food };
+        return game_session{points : 0 , record :0 , player :cobra ,ground : land};
+
     }
 }
 
